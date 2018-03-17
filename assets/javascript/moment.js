d0d1e7e5c99fbb09d917ba5ba91d2055e99c2923
Alex.Subak <asubak@kent.edu>
2018-03-17 08:34:47 -0400

    Rough draft

---
+++ assets/javascript/moment.js
@@ -0,0 +1,3 @@
+// Need to watch how to incorporate Moment.js into
+// projects again. If I remember, it's basically a
+// copy and paste kind of thing from the website. 
---
+++ assets/javascript/train.js
@@ -0,0 +1,24 @@
+// Initialize Firebase
+  //Firebase database info is below. 
+  var config = {
+    apiKey: "AIzaSyBAUw0nrQrx4F2Rx7eKjdRwIAHRUHmNR-4",
+    authDomain: "train-schedule-9df90.firebaseapp.com",
+    databaseURL: "https://train-schedule-9df90.firebaseio.com",
+    projectId: "train-schedule-9df90",
+    storageBucket: "train-schedule-9df90.appspot.com",
+    messagingSenderId: "355094806565"
+  };
+  firebase.initializeApp(config);
+
+  //Set up a jQuery click function for New Train section.
+  //Add variables that would target the four input boxes.
+  //Push that data to the Train Schedule. 
+
+  //Moment.js is connected here somehow to keep a running
+  //tab on the time. 
+
+  //Saw good ideas online of how to incorporate the awesome
+  //fonts refernce link to make a open/close input area. 
+  //Research more. 
+
+  
